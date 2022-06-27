872a1e34e5e7652be8a626c75bc5f36433a964f4
StoneUS <stoneus@em.ail>
2022-06-27 12:13:25 +0300

    e5

--- unit1/exercises/e5/main.go
+++ unit1/exercises/e5/main.go
@@ -1,8 +1,27 @@
 package main
 
-import "golang.org/x/tour/pic"
+import (
+	"image"
+	"image/color"
+
+	"golang.org/x/tour/pic"
+)
 
 type Image struct{}
+
+func (i Image) ColorModel() color.Model {
+	return color.RGBAModel
+}
+
+func (i Image) Bounds() image.Rectangle {
+	return image.Rect(0, 0, 256, 256)
+}
+
+func (i Image) At(x, y int) color.Color {
+	var r, g, b, a uint8 = 25, 0, 0, 0
+	b = uint8(x) * uint8(y)
+	return color.RGBA{r, g, b, a}
+}
 
 func main() {
 	m := Image{}
