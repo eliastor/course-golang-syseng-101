d6f6752242df4c31c64d12172b5a689990a0ad8d
Anton Evtikhov <anton.evtikhov@gmail.com>
2022-07-26 12:15:08 +0500

    unit2/e2

--- unit2/exercises/e2/main.go
+++ unit2/exercises/e2/main.go
@@ -6,8 +6,6 @@ import (
 )
 
 type Fetcher interface {
-	// Fetch returns the body of URL and
-	// a slice of URLs found on that page.
 	Fetch(url string) (body string, urls []string, err error)
 }
 
