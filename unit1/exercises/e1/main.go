8e40522c8fdf6b3cd45ce44ad3350c4cc47f0f92
Mycron <vshpanskiy@gmail.com>
2022-10-20 01:08:31 +0600

    Clear the code with closure

--- unit1/exercises/e1/main.go
+++ unit1/exercises/e1/main.go
@@ -2,26 +2,31 @@ package main
 
 import "fmt"
 
+// fibonacci is a function that returns
 // a function that returns an int.
 func fibonacci() func() int {
-	a, b := 0, 0
+	x := 0
+	y := x
+	z := x
 	return func() int {
-		switch a {
-		case 0:
-			a = 1
-			b = 1
+		if z == 0 {
+			z = 1
 			return 0
-		default:
-			current := a
-			a, b = b, a+b
-			return a - current
+		} else if y == 0 {
+			z = z + y
+			y = 1
+		} else {
+			z = x + y
+			x = y
+			y = z
 		}
+		return z
 	}
 }
 
 func main() {
 	f := fibonacci()
-	for i := 0; i < 12; i++ {
+	for i := 0; i < 15; i++ {
 		fmt.Println(f())
 	}
 }
