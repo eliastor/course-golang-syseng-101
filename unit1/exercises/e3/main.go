1ecacf5634f553a429a6ca573ff7d7a2a2a79a28
Mycron <vshpanskiy@gmail.com>
2022-10-31 19:32:01 +0600

    Correct error output

--- unit1/exercises/e3/main.go
+++ unit1/exercises/e3/main.go
@@ -7,16 +7,16 @@ import (
 type ErrNegativeSqrt float64
 
 func (e ErrNegativeSqrt) Error() string {
-	return fmt.Sprintln("cannot Sqrt negative number: -2")
+	return fmt.Sprintf("cannot Sqrt negative number: %v", float64(e))
 }
 
 func Sqrt(x float64) (float64, error) {
-	var e ErrNegativeSqrt
+	//var e ErrNegativeSqrt
 	epsilon := 0.0000000000001
 	z := x / 2
 	i := 1
 	if x < 0 {
-		return 0, e
+		return 0, ErrNegativeSqrt(x)
 	}
 	for {
 		delta := (z*z - x) / (2 * z)
