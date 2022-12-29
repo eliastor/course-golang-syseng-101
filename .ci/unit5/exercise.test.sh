aea543ecd9dd71b6bd549b1cf8782c505212a009
eliastor <eliastor@users.noreply.github.com>
2022-12-29 10:58:39 +0300

    unit5 materials

---
+++ .ci/unit5/exercise.test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+UNITN="5"
+rm -f "../unit${UNITN}/exercises/e$1/main_test.go"
+cp "unit${UNITN}/e$1_main_test.go.tpl" "../unit${UNITN}/exercises/e$1/main_test.go"
+
+cd ..
+
+go mod init course || true
+go get github.com/stretchr/testify/assert
+
+CGO_ENABLED=0 go test "./unit${UNITN}/exercises/e$1/..."
